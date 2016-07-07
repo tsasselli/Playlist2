c930df92a8d39600fdd3cae773f5d5bc2a7cf8cf
tsasselli <tsasselli@gmail.com>
2016-07-07 13:53:56 -0600

    finished song controller

---
+++ SongController.swift
@@ -0,0 +1,22 @@
+//
+//  SongController.swift
+//  Playlist
+//
+//  Created by Travis Sasselli on 7/7/16.
+//  Copyright © 2016 devMountain. All rights reserved.
+//
+
+import Foundation
+
+
+class SongController {
+
+    func createSong (name:String, artist: String, playlist: Playlist)  {
+        let song = Song(title: name, artist: artist)
+        PlaylistController.sharedController.addSongToPlaylist(song, playlist: playlist)
+        
+    
+    
+    }
+
+}
